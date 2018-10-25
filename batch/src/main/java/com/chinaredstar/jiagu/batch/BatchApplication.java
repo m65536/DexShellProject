61cd14423dcb63b54d88ecdc4ec4cc3b0bf33093
xingwang.mo <xingwang.mo@chinaredstar.com>
2018-10-25 15:37:54 +0800

    11

---
+++ batch/src/main/java/com/chinaredstar/jiagu/batch/BatchApplication.java
@@ -0,0 +1,13 @@
+package com.chinaredstar.jiagu.batch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BatchApplication {
+    private static final Logger logger = LoggerFactory.getLogger(BatchApplication.class);
+
+
+    public static void main(String[] args) {
+        logger.info("main started!");
+    }
+}
---
+++ batch/src/test/java/com/chinaredstar/jiagu/batch/BatchApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chinaredstar.jiagu.batch;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BatchApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(BatchApplicationTests.class);
+
+    @Test
+    public void contextLoads() {
+        logger.info("main test");
+    }
+
+}
